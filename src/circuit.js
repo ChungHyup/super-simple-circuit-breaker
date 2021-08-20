15dbf333e3b76f6f97702fd2d03dac019238c9c8
오충협 <tractar0213@gmail.com>
2021-08-20 14:30:55 +0900

    add default status

--- src/circuit.js
+++ src/circuit.js
@@ -1,5 +1,21 @@
+
+const STATUS = Symbol('status');
+
+const CIRCUIT_STATUS = {
+  OPEN: 0,
+  HALFOPEN: 1,
+  CLOSED: 2
+}
+
 class SimpleCircuitBreaker {
 
+  constructor() {
+    this[STATUS] = CIRCUIT_STATUS.OPEN
+  }
+
+  getCurrentStatus() {
+    return this[STATUS]
+  }
 }
 
 module.exports = exports = SimpleCircuitBreaker;
--- test/test.js
+++ test/test.js
@@ -4,9 +4,13 @@ var SimpleCircuitBreaker = require('../index')
 
 describe('Circuit', function() {
   describe('Create', function() {
+    const circuit = new SimpleCircuitBreaker();
     it('Create Circuit Breaker instance', function() {
-      const circuit = new SimpleCircuitBreaker();
       expect(circuit).to.be.a('object')
+    });
+
+    it('Default Status Shoud be OPEN', function() {
+      expect(circuit.getCurrentStatus()).to.equal(0)
     });
   });
 });
